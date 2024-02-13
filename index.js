42b08bfd78280183d1581b12b4355f016a9f914b
William Klos <wjklos@gmail.com>
2024-02-13 08:21:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!  This is an AWS App Runner-based app.');
 });
 
 app.listen(port, () => {
